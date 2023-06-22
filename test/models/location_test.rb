3df1159614d97197058835cdf1825a021ea982f8
Miray Karabulut <mimikarabulut@gmail.com>
2023-06-22 10:33:42 -0500

    add index action test to tests file

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/locations.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Location.count, data.length
+  end
 end
